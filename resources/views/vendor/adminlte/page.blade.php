809263e7510f154fb7b37b55f999047bff989f79
Adnan Yousaf <38159352+Adnanyousafawan@users.noreply.github.com>
2019-06-25 06:31:03 +0500

    profile image link working

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -173,16 +173,17 @@
                                             <!-- Menu Toggle Button -->
                                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                                 <!-- The user image in the navbar-->
-{{--                                                <img src="{{ Auth::user()->profile_image }}" class="user-image"--}}
-{{--                                                     alt="User Image">--}}
+                                                <img src="/storage/{{ Auth::user()->profile_image }}" class="user-image"
+                                                     alt="User Image">
                                                 <!-- hidden-xs hides the username on small devices so only the image appears. -->
                                                 <span class="hidden-xs">{{ Auth::user()->name }}</span>
                                             </a>
                                             <ul class="dropdown-menu">
                                                 <!-- The user image in the menu -->
                                                 <li class="user-header">
-                                                    <img src="{{-- https://almsaeedstudio.com/themes/AdminLTE/dist/img/user2-160x160.jpg" --}}"
-                                                         class="img-circle" alt="User Image">
+                                                     <img src="/storage/{{ Auth::user()->profile_image }}" class="user-image"
+                                                     alt="User Image">
+                                                       
 
                                                     <p>
                                                         {{ Auth::user()->name }}
